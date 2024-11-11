103bcd36be35c1c1db3d155e4c1d88178ae49403
admin <qazws0606@naver.com>
2024-11-11 18:01:22 +0900

    11/11

--- src/main/java/org/example/pjt/controller/TestController.java
+++ src/main/java/org/example/pjt/controller/TestController.java
@@ -1,4 +1,13 @@
 package org.example.pjt.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TestController {
+
+    @GetMapping("/test1")
+    public String test1() {
+        return "main";
+    }
 }
